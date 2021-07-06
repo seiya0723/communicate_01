e07d608438536905e18df557242f307255499ff9
Seiya Asahina <seiya@asahina>
2021-07-06 17:58:53 +0900

    add

--- bbs/urls.py
+++ bbs/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name    = "bbs"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:pk>', views.single, name="single"),
+    path('<int:pk>/', views.single, name="single"),
 ]
 
